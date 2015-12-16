3f2699013ff3830b31eca1a895f74373277837fd
Twllerm <skyteether@gmail.com>
2015-12-16 03:36:12 +0300

    auto concat js files

--- Front-End/WebApp/gulpfile.js
+++ Front-End/WebApp/gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('public/stylesheets/'))
 });
+gulp.task('scripts', function() {
+  return gulp.src('public/js/*.js')
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('public/javascripts/'));
+});
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function() {
+    gulp.watch('sass/*',['styles']);
+    gulp.watch('public/js/*.js',['scripts']);
 });
+
+
--- Front-End/WebApp/public/javascripts/build.js
+++ Front-End/WebApp/public/javascripts/build.js
@@ -20,3 +20,19 @@ angular.module("extendedMovie.controllers", [])
       })
     }
 }]); 
+
+ var extendedMovie = angular.module("extendedMovie", ['ngRoute', 'extendedMovie.controllers'])
+
+.config(["$routeProvider", function ($routeProvider) {
+  $routeProvider.when("/movies", {
+    templateUrl: "/partials/profile_movies.html",
+    controller: "MoviesCtrl"
+  })
+    .when('/cinemas', {
+          templateUrl: "/partials/profile_cinemas.html",
+          controller: "MoviesCtrl"
+
+          })
+
+}]);
+
