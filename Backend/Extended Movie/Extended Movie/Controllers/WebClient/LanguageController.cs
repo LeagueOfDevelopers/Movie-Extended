9f37712581a83b51fc33440e736cdac72debe249
holymosh <skyland7494@mail.ru>
2015-12-26 23:10:01 +0300

    languageControlller for front end

---
+++ Backend/Extended Movie/Extended Movie/Controllers/WebClient/LanguageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Extended_Movie.Controllers.WebClient
+{
+    public class LanguageController
+    {
+    }
+}
