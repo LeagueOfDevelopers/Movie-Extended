5a96ff6b293e894f2d078af45b8a998b420acb23
holymosh <skyland7494@mail.ru>
2015-12-26 19:22:52 +0300

    MovieRepository

---
+++ Backend/Extended Movie/Extended Movie/Visitor Repository/MovieRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Extended_Movie.Models;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Extended_Movie.Visitor_Repository
+{
+    public class MovieRepository:IMovieRepository
+    {
+        private readonly ISession session;
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return session.Query<Movie>();
+        }
+
+        public Movie GetMovieByMovieId(Guid? movieId)
+        {
+            return session.Query<Movie>().SingleOrDefault(movie => movie.Id == movieId);
+        }
+
+        public Movie GetMovieByCinemaId(Guid cinemaId)
+        {
+            return session.Query<Movie>().SingleOrDefault(movie => movie._cinemaId == cinemaId);
+        }
+
+        public void DeleteMovieByMovieId(Guid? movieId)
+        {
+            var checkIfExists = session.Query<Movie>().SingleOrDefault(movie => movie.Id == movieId);
+            if (checkIfExists != null) session.Delete(checkIfExists);
+        }
+
+        public void DeleteMovieByCinemaId(Guid cinemaId)
+        {
+            var checkIfExists = session.Query<Movie>().Where(movie => movie._cinemaId == cinemaId);
+            if (checkIfExists != null) session.Delete(checkIfExists);
+        }
+    }
+}
