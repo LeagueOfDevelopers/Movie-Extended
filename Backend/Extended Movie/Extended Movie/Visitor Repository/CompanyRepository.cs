d71222337c60c994f8be8e8bd2cdd2c32ddbedb9
holymosh <skyland7494@mail.ru>
2015-12-26 19:13:31 +0300

    CompanyRepository  - Ready

---
+++ Backend/Extended Movie/Extended Movie/Visitor Repository/CompanyRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Extended_Movie.Models;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Extended_Movie.Visitor_Repository
+{
+    public class CompanyRepository :ICompanyRepository
+    {
+        private readonly ISession session;
+        public IEnumerable<Company> GetAllCompanies()
+        {
+            return session.Query<Company>();
+        }
+
+        public Company GetCompanyByCompanyId(Guid? companyId)
+        {
+            return session.Query<Company>().SingleOrDefault(company => company.Id == companyId);
+        }
+
+        public void deleteCompanyById(Guid? companyId)
+        {
+            var checkIfExists = session.Query<Company>().SingleOrDefault(company => company.Id == companyId);
+            if (checkIfExists != null) session.Delete(checkIfExists);
+        }
+    }
+}
