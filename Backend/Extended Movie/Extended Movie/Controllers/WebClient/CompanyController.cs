e780f0721ef1279f48b88cac99a63fad91e94c34
holymosh <skyland7494@mail.ru>
2015-12-28 17:33:34 +0300

    CompanyController
    dd

--- Backend/Extended Movie/Extended Movie/Controllers/WebClient/CompanyController.cs
+++ Backend/Extended Movie/Extended Movie/Controllers/WebClient/CompanyController.cs
@@ -2,10 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Extended_Movie.Models;
+using Extended_Movie.Visitor_Repository;
+using Newtonsoft.Json;
 
 namespace Extended_Movie.Controllers.WebClient
 {
-    public class CompanyController
+    public class CompanyController:ApiController
     {
+        private readonly CompanyRepository companyRepository;
+
+        public CompanyController()
+        {
+            companyRepository = new CompanyRepository();
+        }
+
+        [Route("api/Company/All")]
+        [HttpGet]
+        public IEnumerable<Company> GetAlLCompanies()
+        {
+            return companyRepository.GetAllCompanies();
+        }
+
+        [Route("api/Company/New/{json}")]
+        [HttpGet]
+        public void SaveNewCompany(string json)
+        {
+            var newCompany = JsonConvert.DeserializeObject<Company>(json);
+            companyRepository.SaveCompany(newCompany);
+        }
+
+        [Route("api/Company/Get/{companyId}")]
+        [HttpGet]
+        public Company GetCompanyById(Guid? companyId)
+        {
+            return companyRepository.GetCompanyByCompanyId(companyId);
+        }
+
+        [Route("api/Company/Delete/{companyId")]
+        [HttpGet]
+        public void DeleteCompanyById(Guid? companyId)
+        {
+            companyRepository.deleteCompanyById(companyId);
+        }
     }
 }
