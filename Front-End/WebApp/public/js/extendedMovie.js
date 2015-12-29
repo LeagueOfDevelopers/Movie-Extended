f0fda97d7d23fbe10a723dc336d42ce9a39949e6
Twllerm <skyteether@gmail.com>
2015-12-29 19:23:06 +0300

    поправил ссылочки

--- Front-End/WebApp/public/js/extendedMovie.js
+++ Front-End/WebApp/public/js/extendedMovie.js
@@ -1,25 +1,29 @@
- var extendedMovie = angular.module("extendedMovie", ['ui.router', 'extendedMovie.controllers'])
+ var MovieExtended= {
+    Main: angular.module("MovieExtended.Main", ['ui.router', 'extendedMovie.controllers']),
+    Profile: angular.module("MovieExtended.Profile", ['ui.router', 'extendedMovie.controllers'])
+  };
 
-.config(["$stateProvider","$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
-  $stateProvider.state("movies", {
-    url: "/movies",
-    templateUrl: "/partials/profile_movies.html",
-    controller: "MoviesCtrl"
-  })
-      .state("movies.create_movie", {
-        url:"/movies",
-        templateUrl: "/partials/create_movie.html"
-      })
-      .state("movies.about_movie", {
-        url:"/movies",
-        templateUrl: "/partials/about_movie.html"
-      })
-    .state('cinemas', {
-          url: "/cinemas",
-          templateUrl: "/partials/profile_cinemas.html",
-          controller: "MoviesCtrl"
+  MovieExtended.Profile.config(["$stateProvider","$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+  
+    $stateProvider.state("movies", {
+      url: "/movies",
+      templateUrl: "/partials/profile_movies.html",
+      controller: "MoviesCtrl"
+    })
+        .state("movies.create_movie", {
+          url:"/movies",
+          templateUrl: "/partials/create_movie.html"
+        })
+        .state("movies.about_movie", {
+          url:"/movies",
+          templateUrl: "/partials/about_movie.html"
+        })
+      .state('cinemas', {
+            url: "/cinemas",
+            templateUrl: "/partials/profile_cinemas.html",
+            controller: "MoviesCtrl"
 
-        });
-    $urlRouterProvider.otherwise("/movies");
+          });
+      $urlRouterProvider.otherwise("/movies");
 
 }]);
--- Front-End/WebApp/routes/index.js
+++ Front-End/WebApp/routes/index.js
@@ -3,13 +3,13 @@ var api = require('../internal/api')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('profile');
-});
-
-
 
 router.get('/registry', function(req, res, next) {
-  res.render('registry');
+  res.render('registry', {ngModule:"MovieExtended.Main"});
 });
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', {ngModule: "MovieExtended.Profile"});
+});
+
 module.exports = router;
