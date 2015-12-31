d0c7278c88a37abf548f042e29e6325ec293172e
holymosh <skyland7494@mail.ru>
2015-12-31 15:05:06 +0300

    bootstraper но не полностью

---
+++ Backend/Extended Movie/Extended Movie/App_Start/Bootstraper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Extended_Movie.Visitor_Repository;
+using SimpleInjector;
+
+namespace Extended_Movie.App_Start
+{
+    public class Bootstraper : System.Web.HttpApplication 
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var container = new Container();
+            container.Register<ILanguageRepository,LanguageRepository>(Lifestyle.Singleton);
+            container.Register<IMovieRepository,MovieRepository>(Lifestyle.Singleton);
+            container.Register<IFileRepository,FileRepository>(Lifestyle.Singleton);
+            container.Register<ICinemaRepository,CinemaRepository>(Lifestyle.Singleton);
+            container.Register<ICompanyRepository,CompanyRepository>(Lifestyle.Singleton);
+            container.Verify();
+        }
+    }
+}
