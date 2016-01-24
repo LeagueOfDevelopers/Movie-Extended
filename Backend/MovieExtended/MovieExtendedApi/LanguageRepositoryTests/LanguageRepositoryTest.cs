5da790c6355be8c4f93a69b50ce767d2d4fa5164
holymosh <skyland7494@mail.ru>
2016-01-25 00:09:48 +0300

    слил тестики в отдельную папочку, создал проект для теста languagerepository

---
+++ Backend/MovieExtended/MovieExtendedApi/LanguageRepositoryTests/LanguageRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LanguageRepositoryTests
+{
+    [TestClass]
+    public class LanguageRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
