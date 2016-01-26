857fbbf9b786a8485bb6c6ff7ac688ea5b14415f
holymosh <skyland7494@mail.ru>
2016-01-26 16:11:24 +0300

    поменял Id в сущностях на string т к guid вроде не нужен  и в Id в бд используются не только цифры

---
+++ Backend/MovieExtended/MovieExtendedApi/FileRepositoryTests/FileRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain.Models;
+using Extended_Movie.Visitor_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileRepositoryTests
+{
+    [TestClass]
+    public class FileRepositoryTests
+    {
+        [TestMethod]
+        public void SaveFileData()
+        {
+            var saveFile = new File(Guid.NewGuid(),"filePath",FileType.Track);
+            
+            
+            var provider = new SessionProvider();
+            provider.OpenSession();
+            using (var session = provider.GetCurrentSession())
+            {
+                var fileRepository = new FileRepository(session);
+                fileRepository.SaveFileData(saveFile);
+            }
+        }
+    }
+}
