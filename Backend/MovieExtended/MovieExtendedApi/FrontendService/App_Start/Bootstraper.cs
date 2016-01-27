5573e364fdd503eb94af43108649d61b54878916
holymosh <skyland7494@mail.ru>
2016-01-27 14:19:43 +0300

    Bootstraper

---
+++ Backend/MovieExtended/MovieExtendedApi/FrontendService/App_Start/Bootstraper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Extended_Movie.Visitor_Repository;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+
+namespace FrontendService.App_Start
+{
+    public class Bootstraper
+    {
+        public Container Configure()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
+            container.Register<IMovieRepository>(()=>container.GetInstance<MovieRepository>(),Lifestyle.Singleton);
+            container.Register<ILanguageRepository>(() => container.GetInstance<LanguageRepository>(), Lifestyle.Singleton);
+            container.Register<ICompanyRepository>(() => container.GetInstance<CompanyRepository>(), Lifestyle.Singleton);
+            container.Register<IFileRepository>(() => container.GetInstance<FileRepository>(), Lifestyle.Singleton);
+            container.Register<ICinemaRepository>(() => container.GetInstance<CinemaRepository>(), Lifestyle.Singleton);
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+            container.Verify();
+            return container;
+        }
+    }
+}
