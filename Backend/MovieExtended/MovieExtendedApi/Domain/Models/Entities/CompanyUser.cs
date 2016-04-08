a2302cdeafe08fe779bab696400d68d92d08bfcb
holymosh <skyland7494@mail.ru>
2016-04-08 12:29:51 +0300

    CinemaUser entity

---
+++ Backend/MovieExtended/MovieExtendedApi/Domain/Models/Entities/CompanyUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+     public class CompanyUser
+    {
+         public CompanyUser(int id , string firstname,string lastname , Company company)
+         {
+             Id = id;
+             FirstName = firstname;
+             LastName = lastname;
+             Company = company;
+         }
+
+         protected CompanyUser()
+         {
+             
+         }
+        public virtual int Id { get;  set; }
+        public virtual string FirstName { get; set; }
+        public virtual  string LastName { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
