3984e9cd1f244ae01964c5b2c98d0aebf773ca57
holymosh <skyland7494@mail.ru>
2016-05-05 19:15:12 +0300

    code refactor

--- Backend/MovieExtended/MovieExtendedApi/Domain/FingerPrinting/FingerPrintKeeper.cs
+++ Backend/MovieExtended/MovieExtendedApi/Domain/FingerPrinting/FingerPrintKeeper.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.IO;
 using Domain.Models.Entities;
 using NAudio.Wave;
-using NHibernate.Criterion;
 using SoundFingerprinting;
 using SoundFingerprinting.Audio;
 using SoundFingerprinting.Audio.NAudio;
@@ -13,53 +11,60 @@ using SoundFingerprinting.InMemory;
 
 namespace Domain.FingerPrinting
 {
-  public  class FingerPrintKeeper:IFingerPrintKeeper
+    public class FingerPrintKeeper : IFingerPrintKeeper
     {
-      public FingerPrintKeeper()
-      {
-          _modelService = new InMemoryModelService();
-          _audioService = new NAudioService();
+        private readonly IAudioService _audioService;
+        private readonly IFingerprintCommandBuilder _fingerprintCommandBuilder;
+        private readonly IModelService _modelService;
+
+
+        private FingerprintConfiguration fingerprintConfiguration;
+        private QueryConfiguration queryConfiguration;
+
+        public FingerPrintKeeper()
+        {
+            _modelService = new InMemoryModelService();
+            _audioService = new NAudioService();
             _fingerprintCommandBuilder = new FingerprintCommandBuilder();
             SetFingerPrintConfiguration();
             SetQueryConfiguration();
-      }
+        }
 
-      private void SetFingerPrintConfiguration()
-      {
+
+        public void CreateHashes(string audiopath, Movie movie)
+        {
+            var reader = new MediaFoundationReader(audiopath);
+            var track = new TrackData(
+                "isrc " + movie.Id,
+                "artist " + movie.Id,
+                "title " + movie.Name,
+                "album " + movie.Id,
+                DateTime.Today.Year,
+                reader.TotalTime.TotalMilliseconds
+                );
+            var trackReference = _modelService.InsertTrack(track);
+            var hashedFingerprints = _fingerprintCommandBuilder
+                .BuildFingerprintCommand()
+                .From(audiopath).
+                WithFingerprintConfig(fingerprintConfiguration)
+                .UsingServices(_audioService)
+                .Hash()
+                .Result;
+
+            _modelService.InsertHashDataForTrack(hashedFingerprints, trackReference);
+        }
+
+        private void SetFingerPrintConfiguration()
+        {
             fingerprintConfiguration = new CustomFingerprintConfiguration();
             fingerprintConfiguration.SampleRate = 44100;
             fingerprintConfiguration.NormalizeSignal = true;
         }
 
-      private void SetQueryConfiguration()
-      {
+        private void SetQueryConfiguration()
+        {
             queryConfiguration = new QueryConfiguration();
             queryConfiguration.MaximumNumberOfTracksToReturnAsResult = 1;
         }
-
-      
-
-      private  FingerprintConfiguration fingerprintConfiguration;
-      private  QueryConfiguration queryConfiguration;
-      private IModelService _modelService;
-      private IAudioService _audioService;
-      private IFingerprintCommandBuilder _fingerprintCommandBuilder;
-
-
-      public void CreateHashes(string audiopath,Movie movie)
-      {
-            MediaFoundationReader reader = new MediaFoundationReader(audiopath);
-            var track = new TrackData(
-                "isrc"+movie.Id,
-                "artist"+movie.Id,
-                "title"+movie.Id,
-                "album"+movie.Id,
-                DateTime.Today.Year,
-                reader.TotalTime.TotalMilliseconds
-                );
-
-      }
-
-      
     }
 }
