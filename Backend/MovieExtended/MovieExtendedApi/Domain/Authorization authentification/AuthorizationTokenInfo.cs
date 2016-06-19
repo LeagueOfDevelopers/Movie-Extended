6c457ebe6a73bf83368ce7ae58d1b7b0aef0666a
holymosh <skyland7494@mail.ru>
2016-06-19 15:10:06 +0300

    берем авторизацию у LoD сайта :I Authorizer , AuthorizationTokenInfo

---
+++ Backend/MovieExtended/MovieExtendedApi/Domain/Authorization authentification/AuthorizationTokenInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Journalist;
+
+namespace Domain.Authorization_authentification
+{
+    class AuthorizationTokenInfo
+    {
+        public AuthorizationTokenInfo(int userId, string token, DateTime creationTime)
+        {
+            Require.Positive(userId, nameof(userId));
+            Require.NotEmpty(token, nameof(token));
+
+            UserId = userId;
+            Token = token;
+            CreationTime = creationTime;
+        }
+
+        public int UserId { get; private set; }
+
+        public string Token { get; private set; }
+
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ Backend/MovieExtended/MovieExtendedApi/Domain/Authorization authentification/IAuthorizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Authorization_authentification
+{
+    interface IAuthorizer
+    {
+        TimeSpan TokenLifeTime { get; }
+
+        AuthorizationTokenInfo GetTokenInfo(string authorizationToken);
+
+        AuthorizationTokenInfo Authorize(string email, Password password);
+    }
+}
