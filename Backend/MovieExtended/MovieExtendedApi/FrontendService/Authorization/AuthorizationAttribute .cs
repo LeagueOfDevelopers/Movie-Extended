472efd8f1b8ae427a0cc36067609fa717e4e5844
holymosh <skyland7494@mail.ru>
2016-06-19 17:08:02 +0300

    MovExtIdentity

---
+++ Backend/MovieExtended/MovieExtendedApi/FrontendService/Authorization/AuthorizationAttribute .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace FrontendService.Authorization
+{
+    public class AuthorizationAttribute : AuthorizeAttribute
+    {
+        private readonly AccountRole _accountRole;
+
+        public AuthorizationAttribute(AccountRole accountRole)
+        {
+            _accountRole = accountRole;
+        }
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            return Thread.CurrentPrincipal.IsInRole(_accountRole);
+        }
+    }
+}
---
+++ Backend/MovieExtended/MovieExtendedApi/FrontendService/Authorization/MovExtIdentity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace FrontendService.Authorization
+{
+    public class MovExtIdentity: IIdentity
+    {
+        public MovExtIdentity(int userId, bool isAuthenticated)
+        {
+            UserId = userId;
+            IsAuthenticated = isAuthenticated;
+        }
+
+        public int UserId { get; }
+
+        public static MovExtIdentity EmptyIdentity => new MovExtIdentity(0, false);
+
+        public string Name => UserId.ToString();
+
+        public string AuthenticationType => "Token";
+
+        public bool IsAuthenticated { get; }
+    }
+}
